3bb3f92d6d0bfbdf5442a9a30844e3dec72eb499
xzy <ziyix.21@intl.zju.edu.cn>
2022-04-27 17:48:36 +0800

    practical11

---
+++ Practical11/Alignment.py
@@ -0,0 +1,84 @@
+random=open("C:/Users/xzy/IBI1_2021-22/Practical11/RandomSeq(1).fa")
+human=open("C:/Users/xzy/IBI1_2021-22/Practical11/DLX5_human.fa")
+mouse=open("C:/Users/xzy/IBI1_2021-22/Practical11/DLX5_mouse.fa")
+def percentage_identity(seq1, seq2):
+    count=0
+    for i in range(len(seq1)):
+        if seq1[i]==seq2[i]:
+            count=count+1
+    percent=count/len(seq1)*100
+    return percent
+for line in human:
+    if not line.startswith(">"):
+        ge_human = line
+        break
+gene_human = ''
+for seq in ge_human:
+    if ((seq>="A") and (seq<="Z")) or (seq=="*"):
+        gene_human = gene_human + str(seq)
+for line in mouse:
+    if not line.startswith(">"):
+        ge_mouse= line
+        break
+gene_mouse = ''
+for seq in ge_mouse:
+    if (seq>="A" and seq<="Z")or seq=="*":
+        gene_mouse = gene_mouse + str(seq)
+for line in random:
+    if not line.startswith(">"):
+        ge_random= line
+        break
+gene_random = ''
+for seq in ge_random:
+    if (seq>="A" and seq<="Z")or seq=="*":
+        gene_random = gene_random + str(seq)
+human_mouse=percentage_identity(gene_mouse,gene_human)
+human_random=percentage_identity(gene_random,gene_human)
+mouse_random=percentage_identity(gene_random,gene_mouse)
+print('The percentage of identical amino acids between random and human is:',human_random,'%')
+print('The percentage of identical amino acids between random and mouse is:',mouse_random,'%')
+print('The percentage of identical amino acids between mouse and human is:',human_mouse,'%')
+amino_acid=['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V','B','Z','X','*']
+BLOSUM62 = [
+            [ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
+            [-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
+            [-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
+            [-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+            [ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4],
+            [-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1, -4],
+            [-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+            [ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1, -4],
+            [-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1, -4],
+            [-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1, -4],
+            [-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1, -4],
+            [-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1, -4],
+            [-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1, -4],
+            [-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1, -4],
+            [-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2, -4],
+            [ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0, -4],
+            [ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0, -4],
+            [-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2, -4],
+            [-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1, -4],
+            [ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1, -4],
+            [-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+            [-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+            [ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1, -4],
+            [-4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4,  1],
+]
+def blo_score(s1,s2):
+    blosum_score=0
+    for i in range(len(gene_random)):
+        index1=amino_acid.index(s1[i])
+        index2=amino_acid.index(s2[i])
+        blosum_score=blosum_score+BLOSUM62[index1][index2]
+    return blosum_score
+x=blo_score(gene_mouse,gene_human)
+y=blo_score(gene_mouse,gene_random)
+z=blo_score(gene_human,gene_random)
+print('The BLOSUM score between mouse and human is:',x)
+print('The BLOSUM score between mouse and random is:',y)
+print('The BLOSUM score between random and human is:',z)
+
+
+
+
