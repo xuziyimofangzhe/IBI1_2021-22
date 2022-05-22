5f949137b47fa1f28c84cf5e744bd4fa7b6b1298
Ziyi <101162009+xuziyimofangzhe@users.noreply.github.com>
2022-05-22 19:55:32 +0800

    Update get_cut_genes.py

--- Practical8/get_cut_genes.py
+++ Practical8/get_cut_genes.py
@@ -1,5 +1,5 @@
-f=open("C:/Users/xzy/IBI1_2021-22/Practical8/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa")
-fout = open('C:/Users/xzy/IBI1_2021-22/Practical8/cut_genes.fa','w')
+f=open("Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa")
+fout = open('cut_genes.fa','w')
 EcoRI='GAATTC'
 gene_order=0
 seqs=[]
